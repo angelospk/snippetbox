815ceba3e0914dd328bd3d79256fa2f0cfe1cec4
angelospk <rovas450@gmail.com>
2024-02-06 04:28:16 +0200

    added requestlogger middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 )
 
+// secureHeaders creates a middleware to set secure headers for HTTP responses.
+//
+// It takes a http.Handler as input and returns a http.Handler.
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Note: This is split across multiple lines for readability. You don't
@@ -14,6 +17,16 @@ func secureHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// logRequest logs the incoming HTTP request.
+//
+// It takes a http.Handler as a parameter and returns a http.Handler.
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
 		next.ServeHTTP(w, r)
 	})
 }
